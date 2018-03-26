30d598772df5edf9a888980ae9c4c11958377ad2
asaiz <agustinsaiz02@gmail.com>
2018-03-26 13:00:14 -0300

    add models structures

--- events/models.py
+++ events/models.py
@@ -1,36 +1,49 @@
-# # -*- coding: utf-8 -*-
-# from __future__ import unicode_literals
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-# from django.db import models
+from django.db import models
+from django.contrib.auth import get_user_model
+import datetime
+from django.conf import settings
 
-# # docs model django 1.11
-# # https://docs.djangoproject.com/en/1.11/topics/db/models/
-
-
-# class UserModel(models.Model):
-#     # By default, Django gives each model the following field:
-#     # auto-incrementing primary key.
-#     # id = models.AutoField(primary_key=True)
-#     field_name = models.CharField(max_length=30)
+# docs model django 1.11
+# https://docs.djangoproject.com/en/1.11/topics/db/models/
 
 
-# class EventModel(models.Model):
-#     # Many-to-one relationships
-#     # user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
-#     pass
+class Event(models.Model):
+    # Many-to-one relationships
+    # user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    eventbrite_id = models.CharField(max_length=255, primary_key=True)
+    name = models.CharField(max_length=255)
+    date = models.DateField(default=datetime.date.today)
+    location = models.CharField(max_length=255)
 
 
-# class TicketTypeModel(models.Model):
-#     pass
+class TicketType(models.Model):
+    eventbrite_id = models.CharField(max_length=255, primary_key=True)
+    event = models.ForeignKey(Event)
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    description = models.CharField(max_length=255)
 
 
-# class CustomizationModel(models.Model):
-#     pass
+class TicketTemplate(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    changed = models.DateTimeField(auto_now=True)
 
 
-# class TicketTemplateModel(models.Model):
-#     pass
+class EmailConfirmation(models.Model):
+    message = models.CharField()
+    created = models.DateTimeField(auto_now_add=True)
+    changed = models.DateTimeField(auto_now=True)
 
 
-# class EmailConfirmationModel(models.Model):
-#     pass
+class Customization(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    ticket_template = models.ForeignKey(TicketTemplate)
+    email_confirmation = models.ForeignKey(EmailConfirmation)
+    created = models.DateTimeField(auto_now_add=True)
+    changed = models.DateTimeField(auto_now=True)
