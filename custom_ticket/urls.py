d6fe5869bcef3f195f61899082bd209157c97118
fcarabelli <fcarabelli@outlook.com>
2018-03-27 13:01:31 -0300

    Add template customization_list

--- custom_ticket/urls.py
+++ custom_ticket/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth.views import login, logout
 from .views import IndexView
+from events.views import HomeView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -12,5 +13,5 @@ urlpatterns = [
     url(r'^accounts/login/$', login, name='login'),
     url(r'^accounts/logout/$', logout, name='logout'),
     url(r'^password_reset/$', login, name='password_reset'),
-
+    url(r'^home/', HomeView.as_view()),
 ]
