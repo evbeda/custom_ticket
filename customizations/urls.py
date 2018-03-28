7b12b4d67f4be281a79f348429ef1dd5093e7630
PKpacheco <pkcpweb@gmail.com>
2018-03-28 12:30:48 -0300

    fix urls

--- customizations/urls.py
+++ customizations/urls.py
@@ -12,11 +12,9 @@ urlpatterns = [
     url(r'^send_mail_with_pdf/$', send_mail_with_pdf, name='send_email_with_pdf'),
     url(r'^sent-mail-success/$', TemplateView.as_view(
         template_name='customizations/successfully_mail.html'), name='successfully_mail'),
-    url(r'^create-customization/$', TemplateView.as_view(
-        template_name='customizations/create.html'), name='create_customization'),
     url(r'^send-mail/$', TemplateView.as_view(
         template_name='mail/email_send.html'), name='send-mail'),
-    url(r'^$', ViewCreateCustomization.as_view(), name='customizations'),
-    url(r'success', ViewCreateCustomization.as_view(), name='customizations'),
+    url(r'^create-customization/$', ViewCreateCustomization.as_view(), name='create_customizations'),
+    url(r'success', ViewCreateCustomization.as_view(), name='success_customizations'),
 
 ]
