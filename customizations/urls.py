f16b1295c1efdeb5d490d89d98c8f8cf27832ad0
asaiz <agustinsaiz02@gmail.com>
2018-03-28 10:41:47 -0300

    fix-redirect customizations

--- customizations/urls.py
+++ customizations/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     url(r'^send_mail_with_pdf/$', send_mail_with_pdf, name='send_email_with_pdf'),
     url(r'^sent-mail-success/$', TemplateView.as_view(
         template_name='customizations/successfully_mail.html'), name='successfully_mail'),
+    url(r'^create-customization/$', TemplateView.as_view(
+       template_name='customizations/create.html'), name='create_customization'),
 ]
